e998a287465c53dcf34a35010318f762b9f89029
Sparsh Sah <sparshsah@gmail.com>
2020-11-29 22:36:11 -0500

    multiline comment

--- fann/fann.py
+++ fann/fann.py
@@ -73,17 +73,12 @@ def get_ll(p: pd.Series) -> float:
 # ACTIVATION FUNCTION ##################################################################################################
 ########################################################################################################################
 
-# Three common neuron activation functions are
-# logistic (AKA sigmoid), tanh, and ReLU.
-# I like the logistic for (binary) classification tasks,
-# for the slightly (maybe even misleadingly) arbitrary reason
-# that its output lies in [0, 1],
-# so it can be interpreted as this neuron's "best guess"
-# of the probability that the input belongs to Category 1.
-# As further support, the logistic's generalization, the softmax,
-# is a commonly-used "output squashing" function for
-# multinomial classification tasks: It transforms a `n`-vector
-# of Real numbers into a probability mass distribution.
-# Tanh is simply a scaled-and-shifted version of logistic.
-# ReLU is cool too, and has another cool connection,
-# this time to the hinge loss function.
+"""
+Three common neuron activation functions are logistic (AKA sigmoid), tanh, and ReLU.
+I like the logistic for (binary) classification tasks, for the slightly (maybe even misleadingly) arbitrary reason
+that its output lies in [0, 1], so it can be interpreted as this neuron's "best guess"
+of the probability that the input belongs to Category 1. As further support, the logistic's generalization, the softmax,
+is a commonly-used "output squashing" function for multinomial classification tasks: It transforms a `n`-vector
+of Real numbers into a probability mass distribution. (And tanh is simply a scaled-and-shifted version of logistic.)
+ReLU is cool too, and has another cool connection, this time to the hinge loss function.
+"""
