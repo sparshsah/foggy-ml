d401fc3a4c8e11d7eee6ba9798d771dedbff8dcd
Sparsh Sah <sparshsah@gmail.com>
2020-11-30 20:58:30 -0500

    bprop

--- fann/fann.py
+++ fann/fann.py
@@ -274,3 +274,50 @@ def get_loss(y: pd.Series, p_hat: pd.DataFrame) -> float:
 ########################################################################################################################
 # TRAINING AKA BACK-PROPAGATION ########################################################################################
 ########################################################################################################################
+
+
+def bprop():
+    # This function learns parameters for the neural network and returns the model.
+    # - nn_hdim: Number of nodes in the hidden layer
+    # - num_passes: Number of passes through the training data for gradient descent
+    # Initialize the parameters to random values. We need to learn these.
+    nn_hdim = 3
+    num_passes = 20_000
+    np.random.seed(1337)
+    W1 = np.random.randn(nn_input_dim, nn_hdim) / np.sqrt(nn_input_dim)
+    b1 = np.zeros((1, nn_hdim))
+    W2 = np.random.randn(nn_hdim, nn_output_dim) / np.sqrt(nn_hdim)
+    b2 = np.zeros((1, nn_output_dim))
+
+    # This is what we return at the end
+    model = {}
+
+    # Gradient descent. For each batch...
+    for i in range(0, num_passes):
+
+        # Forward propagation
+        z1 = X.dot(W1) + b1
+        a1 = np.tanh(z1)
+        z2 = a1.dot(W2) + b2
+        exp_scores = np.exp(z2)
+        probs = exp_scores / np.sum(exp_scores, axis=1, keepdims=True)
+
+        # Backpropagation
+        delta3 = probs
+        delta3[range(num_examples), y] -= 1
+        dW2 = (a1.T).dot(delta3)
+        db2 = np.sum(delta3, axis=0, keepdims=True)
+        delta2 = delta3.dot(W2.T) * (1 - np.power(a1, 2))
+        dW1 = np.dot(X.T, delta2)
+        db1 = np.sum(delta2, axis=0)
+
+        # Gradient descent parameter update
+        W1 += -epsilon * dW1
+        b1 += -epsilon * db1
+        W2 += -epsilon * dW2
+        b2 += -epsilon * db2
+
+        # Assign new parameters to the model
+        model = { 'W1': W1, 'b1': b1, 'W2': W2, 'b2': b2}
+
+    return model
