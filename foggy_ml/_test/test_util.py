cd56c0d246e35c7901da653b343aa777bc0ee2c2
Sparsh Sah <sparshsah@gmail.com>
2021-01-01 02:53:51 -0500

    test skeleton

---
+++ foggy_ml/_test/test_util.py
@@ -0,0 +1,12 @@
+# project source code
+from ..util import *
+# testing suite
+import unittest
+
+class TestSkeleton(unittest.TestCase):
+
+    def test_skeleton(self):
+        self.assertTrue(True)
+
+if __name__ == "__main__":
+    unittest.main()
