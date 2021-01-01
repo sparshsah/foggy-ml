88d9a6612fded022d5d4ab48de95eaf64c208812
Sparsh Sah <sparshsah@gmail.com>
2021-01-01 15:56:30 -0500

    silence unused wildcard import warning

--- foggy_ml/_test/test_util.py
+++ foggy_ml/_test/test_util.py
@@ -1,7 +1,7 @@
 # project source code
 # assumes `/path/to/foggy-ml` has been added to `PYTHONPATH`
 # preferable `from ..util import *` relative import, since we need to run this as __main__
-from foggy_ml.util import *
+# from foggy_ml.util import *
 # testing suite
 import unittest
 
--- foggy_ml/fann/_test/test_fann.py
+++ foggy_ml/fann/_test/test_fann.py
@@ -1,7 +1,7 @@
 # project source code
 # assumes `/path/to/foggy-ml` has been added to `PYTHONPATH`
 # preferable `from ..fann import *` relative import, since we need to run this as __main__
-from foggy_ml.fann import *
+# from foggy_ml.fann import *
 # testing suite
 import unittest
 from unittest import mock
