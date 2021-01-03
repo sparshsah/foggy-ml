74239cdee25c4ead40283c1cadb5ad0ab1dea8eb
Sparsh Sah <sparshsah@gmail.com>
2021-01-03 15:49:32 -0500

    project -> target

--- foggy_ml/_test/test_util.py
+++ foggy_ml/_test/test_util.py
@@ -1,4 +1,4 @@
-# project source code
+# target source code
 # assumes `/path/to/foggy-ml` has been added to `PYTHONPATH`
 # preferred over `from ..util import *` relative import, since we need to run this standalone as __main__
 # from foggy_ml.util import *
