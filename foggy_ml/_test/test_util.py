6bd7c6c533e42b73153b9c95bfe111c2178ca62b
Sparsh Sah <sparshsah@gmail.com>
2021-01-03 15:45:42 -0500

    typo

--- foggy_ml/_test/test_util.py
+++ foggy_ml/_test/test_util.py
@@ -1,6 +1,6 @@
 # project source code
 # assumes `/path/to/foggy-ml` has been added to `PYTHONPATH`
-# preferable `from ..util import *` relative import, since we need to run this as __main__
+# preferred over `from ..util import *` relative import, since we need to run this standalone as __main__
 # from foggy_ml.util import *
 # testing suite
 import unittest
--- foggy_ml/fann/_test/test_fann.py
+++ foggy_ml/fann/_test/test_fann.py
@@ -1,6 +1,6 @@
 # project source code
 # assumes `/path/to/foggy-ml` has been added to `PYTHONPATH`
-# preferable `from ..fann import *` relative import, since we need to run this as __main__
+# preferred over `from ..fann import *` relative import, since we need to run this standalone as __main__
 # from foggy_ml.fann import *
 # testing suite
 import unittest
