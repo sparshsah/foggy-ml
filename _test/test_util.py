48f1b2cf0584354ea729e4eca5d4b5858d059bca
Sparsh Sah <sparshsah@gmail.com>
2021-01-23 19:35:42 -0500

    test_util legit test

--- _test/test_util.py
+++ _test/test_util.py
@@ -1,15 +1,14 @@
-# TODO(sparshsah)
 # target source code
-# assumes `/path/to/foggy-ml` has been added to `PYTHONPATH`
-# from foggy_ml.util import *
+import foggy_ml.util as util  # assumes `/path/to/foggy-ml` in `PYTHONPATH`, so pylint: disable=import-error
 # unit-testing suite
 import unittest
 
 
-class TestSkeleton(unittest.TestCase):
+class TestCheckType(unittest.TestCase):
 
-    def test_skeleton(self):
-        self.assertTrue(True)
+    def test_primitive_int_succ(self):
+        for x in -128, -2, -1, 0, 1, 2, 172:
+            self.assertEqual(util.check_type(x, type_=int), x)
 
 
 if __name__ == "__main__":
