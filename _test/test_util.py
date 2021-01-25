6a7febcb877fc3e0f8e67386943d4e079d5d8b71
Sparsh Sah <sparshsah@gmail.com>
2021-01-24 19:07:35 -0500

    finish type tests

--- _test/test_util.py
+++ _test/test_util.py
@@ -28,6 +28,50 @@ class TestCheckType(unittest.TestCase):
                 util.check_type(x, type_=pd.Series)
 
 
+class TestCheckNotType(unittest.TestCase):
+    """Complement of TestCheckType."""
+
+    def test_primitive_int_fail_direct(self):
+        for x in -128, -2, -1, 0, 1, 2, 172:
+            with self.assertRaises(TypeError):
+                util.check_type(x, type_=int, check_not=True)
+
+    def test_primitive_int_fail_wrapped(self):
+        for x in -128, -2, -1, 0, 1, 2, 172:
+            with self.assertRaises(TypeError):
+                util.check_not_type(x, type_=int)
+
+    def test_primitive_int_succ_direct(self):
+        for x in "a", 3.14, (0, 1), [0, 1]:
+            self.assertEqual(util.check_type(x, type_=int, check_not=True), x)
+
+    def test_primitive_int_succ_wrapped(self):
+        for x in "a", 3.14, (0, 1), [0, 1]:
+            self.assertEqual(util.check_not_type(x, type_=int), x)
+
+    def test_complex_series_fail_direct(self):
+        import pandas as pd
+        for x in pd.Series(), pd.Series([0, 1]):
+            with self.assertRaises(TypeError):
+                util.check_type(x, type_=pd.Series, check_not=True)
+
+    def test_complex_series_fail_wrapped(self):
+        import pandas as pd
+        for x in pd.Series(), pd.Series([0, 1]):
+            with self.assertRaises(TypeError):
+                util.check_not_type(x, type_=pd.Series)
+
+    def test_complex_series_succ_direct(self):
+        import pandas as pd
+        for x in "a", 0, pd.DataFrame(), pd.DataFrame(0, index=["r0", "r1"], columns=["c0", "c1"]):
+            self.assertIs(util.check_type(x, type_=pd.Series, check_not=True), x)
+
+    def test_complex_series_succ_wrapped(self):
+        import pandas as pd
+        for x in "a", 0, pd.DataFrame(), pd.DataFrame(0, index=["r0", "r1"], columns=["c0", "c1"]):
+            self.assertIs(util.check_not_type(x, type_=pd.Series), x)
+
+
 class TestCheckDtype(unittest.TestCase):
 
     def test_non_container_fail(self):
@@ -77,6 +121,94 @@ class TestCheckDtype(unittest.TestCase):
         x = pd.DataFrame({"c0": {"r0": 0, "r1": 1}, "c1": {"r0": 2, "r1": 3.}})
         with self.assertRaises(TypeError):
             util.check_dtype(x, type_=int)
+
+
+class TestCheckNotDtype(unittest.TestCase):
+    """Complement of TestCheckDtype."""
+
+    def test_int_tuple_fail_direct(self):
+        x = (0, 1)
+        with self.assertRaises(TypeError):
+            util.check_type(x, type_=int, check_dtype=True, check_not=True)
+
+    def test_int_tuple_fail_wrapped(self):
+        x = (0, 1)
+        with self.assertRaises(TypeError):
+            util.check_dtype(x, type_=int, check_not=True)
+
+    def test_int_tuple_succ_direct(self):
+        import pandas as pd
+        for x in ("a", "b"), (3.14, 2.71), (0, 0.):
+            self.assertEqual(util.check_type(x, type_=int, check_dtype=True, check_not=True), x)
+
+    def test_int_tuple_succ_wrapped(self):
+        import pandas as pd
+        for x in ("a", "b"), (3.14, 2.71), (0, 0.):
+            self.assertEqual(util.check_dtype(x, type_=int, check_not=True), x)
+
+    def test_int_list_fail_direct(self):
+        x = [0, 1]
+        with self.assertRaises(TypeError):
+            util.check_type(x, type_=int, check_dtype=True, check_not=True)
+
+    def test_int_list_fail_wrapped(self):
+        x = [0, 1]
+        with self.assertRaises(TypeError):
+            util.check_dtype(x, type_=int, check_not=True)
+
+    def test_int_list_succ_direct(self):
+        import pandas as pd
+        for x in ["a", "b"], [3.14, 2.71], [0, 0.]:
+            self.assertEqual(util.check_type(x, type_=int, check_dtype=True, check_not=True), x)
+
+    def test_int_list_succ_wrapped(self):
+        import pandas as pd
+        for x in ["a", "b"], [3.14, 2.71], [0, 0.]:
+            self.assertEqual(util.check_dtype(x, type_=int, check_not=True), x)
+
+    def test_int_series_fail_direct(self):
+        import pandas as pd
+        x = pd.Series([0, 1])
+        with self.assertRaises(TypeError):
+            util.check_type(x, type_=int, check_dtype=True, check_not=True)
+
+    def test_int_series_fail_wrapped(self):
+        import pandas as pd
+        x = pd.Series([0, 1])
+        with self.assertRaises(TypeError):
+            util.check_dtype(x, type_=int, check_not=True)
+
+    def test_int_series_succ_direct(self):
+        import pandas as pd
+        for x in pd.Series(["a", "b"]), pd.Series([3.14, 2.71]), pd.Series([0, 0.]):
+            self.assertIs(util.check_type(x, type_=int, check_dtype=True, check_not=True), x)
+
+    def test_int_series_succ_wrapped(self):
+        import pandas as pd
+        for x in pd.Series(["a", "b"]), pd.Series([3.14, 2.71]), pd.Series([0, 0.]):
+            self.assertIs(util.check_dtype(x, type_=int, check_not=True), x)
+
+    def test_int_df_fail_direct(self):
+        import pandas as pd
+        x = pd.DataFrame({"c0": {"r0": 0, "r1": 1}, "c1": {"r0": 2, "r1": 3}})
+        with self.assertRaises(TypeError):
+            util.check_type(x, type_=int, check_dtype=True, check_not=True)
+
+    def test_int_df_fail_wrapped(self):
+        import pandas as pd
+        x = pd.DataFrame({"c0": {"r0": 0, "r1": 1}, "c1": {"r0": 2, "r1": 3}})
+        with self.assertRaises(TypeError):
+            util.check_dtype(x, type_=int, check_not=True)
+
+    def test_int_df_succ_direct(self):
+        import pandas as pd
+        x = pd.DataFrame({"c0": {"r0": 0, "r1": 1}, "c1": {"r0": 2, "r1": 3.}})
+        self.assertIs(util.check_type(x, type_=int, check_dtype=True, check_not=True), x)
+
+    def test_int_df_succ_wrapped(self):
+        import pandas as pd
+        x = pd.DataFrame({"c0": {"r0": 0, "r1": 1}, "c1": {"r0": 2, "r1": 3.}})
+        self.assertIs(util.check_dtype(x, type_=int, check_not=True), x)
 
 
 if __name__ == "__main__":
