b9026d7f1c77af045ae6db831d37f6244e5550d9
Sparsh Sah <sparshsah@gmail.com>
2021-01-25 00:44:43 -0500

    explain why im ok w/o unit tests for now

--- _test/fann/test_fann.py
+++ _test/fann/test_fann.py
@@ -1,4 +1,4 @@
-# TODO(sparshsah)
+# TODO(sparshsah): for now i'm comfortable since i replicate sklearn, but i should fill this in later
 # target source code
 # assumes `/path/to/foggy-ml` has been added to `PYTHONPATH`
 # from foggy_ml.fann import *
